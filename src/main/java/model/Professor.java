61cc98bd1d7eb1ef5f23ba766d0abd4798c1b97a
Albert Gomes <albert.gomes@liferay.com>
2022-05-18 12:16:45 -0300

    correcao de ideitacao classe professor

--- src/main/java/model/Professor.java
+++ src/main/java/model/Professor.java
@@ -14,102 +14,102 @@ public class Professor implements ProfessorDAO {
             super();
         }
 
-        public double getSalario() {
-            return Salario;
-        }
+                public double getSalario() {
+                    return Salario;
+                }
 
-        public void setSalario(double salario) {
-            Salario = salario;
-        }
+                public void setSalario(double salario) {
+                    Salario = salario;
+                }
 
-        public long getMatricula() {
-            return matricula;
-        }
+                public long getMatricula() {
+                    return matricula;
+                }
 
-        public void setMatricula(long matricula) {
-            this.matricula = matricula;
-        }
+                public void setMatricula(long matricula) {
+                    this.matricula = matricula;
+                }
 
-        public String getNome() {
-            return nome;
-        }
+                public String getNome() {
+                    return nome;
+                }
 
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
+                public void setNome(String nome) {
+                    this.nome = nome;
+                }
 
-        public void AtribuirNota(long matriculaAluno, String disciplinaRef, float atribuirNota, float atribuirNota1, float atribuirNota2, float atribuirNota3) throws Exception {
+                public void AtribuirNota(long matriculaAluno, String disciplinaRef, float atribuirNota, float atribuirNota1, float atribuirNota2, float atribuirNota3) throws Exception {
 
-                    Disciplinas disciplinas = new Disciplinas();
+                        Disciplinas disciplinas = new Disciplinas();
 
-                    Aluno aluno = new Aluno();
+                        Aluno aluno = new Aluno();
 
-                    if(matriculaAluno == aluno.getMatricula()){
+                        if(matriculaAluno == aluno.getMatricula()){
 
-                            if(disciplinaRef.equals(disciplinas.getMatematica())){
+                                if(disciplinaRef.equals(disciplinas.getMatematica())){
 
-                                Notas notaMatematica = new Notas();
+                                    Notas notaMatematica = new Notas();
 
-                                notaMatematica.setNota(atribuirNota);
-                                notaMatematica.setNota1(atribuirNota1);
-                                notaMatematica.setNota2(atribuirNota2);
-                                notaMatematica.setNota3(atribuirNota3);
+                                    notaMatematica.setNota(atribuirNota);
+                                    notaMatematica.setNota1(atribuirNota1);
+                                    notaMatematica.setNota2(atribuirNota2);
+                                    notaMatematica.setNota3(atribuirNota3);
 
-                            }
-                            else if(disciplinaRef.equals(disciplinas.getPortugues())) {
+                                }
+                                else if(disciplinaRef.equals(disciplinas.getPortugues())) {
 
-                                Notas notaPortugues = new Notas();
+                                    Notas notaPortugues = new Notas();
 
-                                notaPortugues.setNota(atribuirNota);
-                                notaPortugues.setNota1(atribuirNota1);
-                                notaPortugues.setNota2(atribuirNota2);
-                                notaPortugues.setNota3(atribuirNota3);
+                                    notaPortugues.setNota(atribuirNota);
+                                    notaPortugues.setNota1(atribuirNota1);
+                                    notaPortugues.setNota2(atribuirNota2);
+                                    notaPortugues.setNota3(atribuirNota3);
 
-                            }
-                            else if(disciplinaRef.equals(disciplinas.getGeografia())) {
+                                }
+                                else if(disciplinaRef.equals(disciplinas.getGeografia())) {
 
-                                Notas notaGeografia = new Notas();
+                                    Notas notaGeografia = new Notas();
 
-                                notaGeografia.setNota(atribuirNota);
-                                notaGeografia.setNota1(atribuirNota1);
-                                notaGeografia.setNota2(atribuirNota2);
-                                notaGeografia.setNota3(atribuirNota3);
+                                    notaGeografia.setNota(atribuirNota);
+                                    notaGeografia.setNota1(atribuirNota1);
+                                    notaGeografia.setNota2(atribuirNota2);
+                                    notaGeografia.setNota3(atribuirNota3);
 
-                            }
-                            else if(disciplinaRef.equals(disciplinas.getCiencias())) {
+                                }
+                                else if(disciplinaRef.equals(disciplinas.getCiencias())) {
 
-                                Notas notasCiencias = new Notas();
+                                    Notas notasCiencias = new Notas();
 
-                                notasCiencias.setNota(atribuirNota);
-                                notasCiencias.setNota1(atribuirNota1);
-                                notasCiencias.setNota2(atribuirNota2);
-                                notasCiencias.setNota3(atribuirNota3);
+                                    notasCiencias.setNota(atribuirNota);
+                                    notasCiencias.setNota1(atribuirNota1);
+                                    notasCiencias.setNota2(atribuirNota2);
+                                    notasCiencias.setNota3(atribuirNota3);
 
-                            }
-                            else if(disciplinaRef.equals(disciplinas.getIngles())) {
+                                }
+                                else if(disciplinaRef.equals(disciplinas.getIngles())) {
 
-                                Notas notasIngles = new Notas();
+                                    Notas notasIngles = new Notas();
 
-                                notasIngles.setNota(atribuirNota);
-                                notasIngles.setNota1(atribuirNota1);
-                                notasIngles.setNota2(atribuirNota2);
-                                notasIngles.setNota3(atribuirNota3);
+                                    notasIngles.setNota(atribuirNota);
+                                    notasIngles.setNota1(atribuirNota1);
+                                    notasIngles.setNota2(atribuirNota2);
+                                    notasIngles.setNota3(atribuirNota3);
 
-                            }
-                            else if(disciplinaRef.equals(disciplinas.getBiologia())) {
+                                }
+                                else if(disciplinaRef.equals(disciplinas.getBiologia())) {
 
-                                Notas notasBiologia = new Notas();
+                                    Notas notasBiologia = new Notas();
 
-                                notasBiologia.setNota(atribuirNota);
-                                notasBiologia.setNota1(atribuirNota1);
-                                notasBiologia.setNota2(atribuirNota2);
-                                notasBiologia.setNota3(atribuirNota3);
+                                    notasBiologia.setNota(atribuirNota);
+                                    notasBiologia.setNota1(atribuirNota1);
+                                    notasBiologia.setNota2(atribuirNota2);
+                                    notasBiologia.setNota3(atribuirNota3);
 
-                            }
+                                }
 
-                    } else {
-                        throw new Exception("Aluno nao existe");
-                    }
+                        } else {
+                            throw new Exception("Aluno nao existe");
+                        }
 
                 }
 
